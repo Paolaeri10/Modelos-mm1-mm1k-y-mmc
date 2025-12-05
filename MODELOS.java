8a636a92ebf1ddd5815857acb844c0556912269d
Paolaeri10 <paoeri10@gmail.com>
2025-12-04 19:56:38 -0600

    Update MODELOS.java

--- MODELOS.java
+++ MODELOS.java
@@ -126,4 +126,9 @@ public class FloreriaAustin {
         // Factorial simple
         public static int factorial(int n) {
         int f = 1;
-        
+        for (int i = 1; i <= n; i++) {
+            f *= i;
+        }
+        return f;
+    }
+}
